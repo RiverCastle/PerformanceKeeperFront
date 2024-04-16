b1a2879d5925d57dec185b7f67d253c3d1146b31
RiverCastle <rkdtjd12@gmail.com>
2024-04-16 11:46:55 +0900

    first commit

---
+++ src/main/java/Performance/PerformanceKeeperFront/PerformanceKeeperFrontApplication.java
@@ -0,0 +1,13 @@
+package Performance.PerformanceKeeperFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceKeeperFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceKeeperFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Performance/PerformanceKeeperFront/PerformanceKeeperFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package Performance.PerformanceKeeperFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceKeeperFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
