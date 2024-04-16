923282d0aa3562f85b602d232e81943dea97a3a0
RiverCastle <rkdtjd12@gmail.com>
2024-04-16 12:32:41 +0900

    feat: ServerCheckController add

---
+++ src/main/java/Performance/PerformanceKeeperFront/frontend/ServerCheckController.java
@@ -0,0 +1,43 @@
+package Performance.PerformanceKeeperFront.frontend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 서버의 상태와 관련된 요청을 처리하는 Controller입니다.
+ * 현재 배포된 서버의 상태를 확인하는 용도로 사용합니다.
+ * 현재 사용하고 있는 포트, 서버의 이름을 조회하는 요청을 처리합니다.
+ * CICD 배포구조를 구축하면서 생성하였습니다.
+ */
+@RestController
+@RequestMapping("/api/server")
+public class ServerCheckController {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String port;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+
+    @GetMapping
+    public ResponseEntity<?> serverCheck() {
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("env", env);
+        responseData.put("portNumber", port);
+        responseData.put("serverAddress", serverAddress);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
+
